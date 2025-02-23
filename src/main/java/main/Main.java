20776a11cd2574d4815d3401c6807590734c6abe
ddavmy <ddavmy@gmail.com>
2025-02-23 05:15:17 +0100

    Display basic window

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        window.setTitle("Unknown Land");
+        window.setSize(1280, 1024);
+    }
+}
