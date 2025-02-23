e10482d20eb873512dd08e704d907281a83f9b2d
ddavmy <ddavmy@gmail.com>
2025-02-23 05:35:09 +0100

    Implementation of game thread

--- src/main/java/main/GameLoop.java
+++ src/main/java/main/GameLoop.java
@@ -3,19 +3,33 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GameLoop extends JPanel {
+public class GameLoop extends JPanel implements Runnable {
     final int originalTileSize = 16; // 16x16 tile
     final int scale = 4;
 
-    final int tileSize =  originalTileSize * scale; // 64x64 tile
+    final int tileSize = originalTileSize * scale; // 64x64 tile
     final int maxScreenCol = 20;
     final int maxScreenRow = 16;
     final int screenWidth = tileSize * maxScreenCol; // 1280 pixels
     final int screenHeight = tileSize * maxScreenRow; // 1024 pixels
 
+    Thread gameThread;
+
     public GameLoop() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while (gameThread.isAlive()) {
+            System.out.println("Game loop started");
+        }
     }
 }
