c777d9ff26902858f8bdc4b12951dcea4f6144be
ddavmy <ddavmy@gmail.com>
2025-02-24 02:04:21 +0100

    Refactored sprite loading and displaying character frames

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -13,12 +13,15 @@ public class Player extends Entity {
 
     GameLoop gl;
     InputHandler inputHandler;
+    BufferedImage[][] sprites = new BufferedImage[4][];
+
+    String[] directions = {"up", "down", "left", "right"};
 
     public Player(GameLoop gl, InputHandler inputHandler) {
         this.gl = gl;
         this.inputHandler = inputHandler;
         setDefaultValues();
-        getPlayerImage();
+        loadPlayerImages();
     }
 
     public void setDefaultValues() {
@@ -28,35 +31,26 @@ public class Player extends Entity {
         direction = "up";
     }
 
-    public void getPlayerImage() {
-        try {
-            up1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/up_1.png")));
-            up2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/up_2.png")));
-            up3 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/up_3.png")));
-            up4 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/up_4.png")));
-            up5 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/up_5.png")));
-            up6 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/up_6.png")));
-            down1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/down_1.png")));
-            down2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/down_2.png")));
-            down3 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/down_3.png")));
-            down4 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/down_4.png")));
-            down5 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/down_5.png")));
-            down6 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/down_6.png")));
-            left1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/left_1.png")));
-            left2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/left_2.png")));
-            left3 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/left_3.png")));
-            left4 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/left_4.png")));
-            left5 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/left_5.png")));
-            left6 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/left_6.png")));
-            right1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/right_1.png")));
-            right2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/right_2.png")));
-            right3 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/right_3.png")));
-            right4 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/right_4.png")));
-            right5 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/right_5.png")));
-            right6 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/walk/right_6.png")));
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void loadPlayerImages() {
+        for (int i = 0; i < directions.length; i++) {
+            String dir = directions[i];
+            sprites[i] = loadDirectionSprites("/player/walk/" + dir + "_");
         }
+    }
+
+    private BufferedImage[] loadDirectionSprites(String pathPrefix) {
+        BufferedImage[] images;
+        int frameCount = 6;
+        images = new BufferedImage[frameCount];
+
+        for (int i = 0; i < frameCount; i++) {
+            try  {
+                images[i] = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(pathPrefix + (i + 1) + ".png")));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return images;
     }
 
     public void update() {
@@ -79,106 +73,20 @@ public class Player extends Entity {
 
         spriteCounter++;
         if (spriteCounter > 6) {
-            if (spriteNumber == 1) {
-                spriteNumber = 2;
-            } else if (spriteNumber == 2) {
-                spriteNumber = 3;
-            } else if (spriteNumber == 3) {
-                spriteNumber = 4;
-            } else if (spriteNumber == 4) {
-                spriteNumber = 5;
-            } else if (spriteNumber == 5) {
-                spriteNumber = 6;
-            } else if (spriteNumber == 6) {
-                spriteNumber = 1;
-            }
+            spriteNumber = (spriteNumber + 1) % sprites[getDirectionIndex()].length;
             spriteCounter = 0;
         }
     }
 
     public void draw(Graphics2D g2) {
-        BufferedImage image = null;
-        switch (direction) {
-            case "up":
-                if (spriteNumber == 1) {
-                    image = up1;
-                }
-                if (spriteNumber == 2) {
-                    image = up2;
-                }
-                if (spriteNumber == 3) {
-                    image = up3;
-                }
-                if (spriteNumber == 4) {
-                    image = up4;
-                }
-                if (spriteNumber == 5) {
-                    image = up5;
-                }
-                if (spriteNumber == 6) {
-                    image = up6;
-                }
-                break;
-            case "down":
-                if (spriteNumber == 1) {
-                    image = down1;
-                }
-                if (spriteNumber == 2) {
-                    image = down2;
-                }
-                if (spriteNumber == 3) {
-                    image = down3;
-                }
-                if (spriteNumber == 4) {
-                    image = down4;
-                }
-                if (spriteNumber == 5) {
-                    image = down5;
-                }
-                if (spriteNumber == 6) {
-                    image = down6;
-                }
-                break;
-            case "left":
-                if (spriteNumber == 1) {
-                    image = left1;
-                }
-                if (spriteNumber == 2) {
-                    image = left2;
-                }
-                if (spriteNumber == 3) {
-                    image = left3;
-                }
-                if (spriteNumber == 4) {
-                    image = left4;
-                }
-                if (spriteNumber == 5) {
-                    image = left5;
-                }
-                if (spriteNumber == 6) {
-                    image = left6;
-                }
-                break;
-            case "right":
-                if (spriteNumber == 1) {
-                    image = right1;
-                }
-                if (spriteNumber == 2) {
-                    image = right2;
-                }
-                if (spriteNumber == 3) {
-                    image = right3;
-                }
-                if (spriteNumber == 4) {
-                    image = right4;
-                }
-                if (spriteNumber == 5) {
-                    image = right5;
-                }
-                if (spriteNumber == 6) {
-                    image = right6;
-                }
-        }
+        BufferedImage image = sprites[getDirectionIndex()][spriteNumber];
         g2.drawImage(image, x, y, gl.tileSize, gl.tileSize, null);
+    }
+
+    private int getDirectionIndex() {
+        for (int i = 0; i < directions.length; i++) {
+            if (directions[i].equals(direction)) return i;
+        }
+        return 0;
     }
 }
